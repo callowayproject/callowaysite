44e7e17d45cc0b0808d4fe908ffd1fc7c7fb612c
Corey Oordt <coreyoordt@gmail.com>
2012-07-03 20:16:22 -0500

    Updated the projects model and added a command to update the projects from github.

---
+++ apps/projects/management/commands/update_projects.py
@@ -0,0 +1,33 @@
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    help = "Update the projects from github"
+
+    def handle(self, *args, **options):
+        from pygithub3 import Github
+        from projects.models import Project
+        from django.conf import settings
+
+        gh = Github(login=settings.GITHUB_USER, password=settings.GITHUB_PASSWORD)
+        repos = gh.repos.list(user='callowayproject').all()
+
+        for repo in repos:
+            try:
+                proj = Project.objects.get(external_id=repo.id)
+                proj.name = repo.name
+                proj.description = repo.description
+                proj.url = repo.html_url
+                proj.is_fork = repo.fork
+                proj.save()
+            except Project.DoesNotExist:
+                proj = Project(
+                    name=repo.name,
+                    description=repo.description,
+                    code_url=repo.html_url,
+                    is_fork=repo.fork,
+                    external_id=repo.id,
+                    project_type=1,
+                    status=1
+                )
+                proj.save()
--- apps/projects/models.py
+++ apps/projects/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.core.files.images import get_image_dimensions
+
 from projects.settings import LOGO_STORAGE, PROJECT_TYPES, STATUSES
 
 
@@ -9,10 +11,24 @@ class Project(models.Model):
     code_url = models.CharField(blank=True, max_length=255)
     docs_url = models.CharField(blank=True, max_length=255)
     logo = models.FileField(blank=True, upload_to='projects/logos', storage=LOGO_STORAGE())
+    logo_width = models.IntegerField(editable=False, blank=True, null=True)
+    logo_height = models.IntegerField(editable=False, blank=True, null=True)
     is_fork = models.BooleanField(default=False)
+    why_forked = models.TextField(blank=True, null=True)
     external_id = models.IntegerField(blank=True, null=True)
-    project_type = models.IntegerField(choices=PROJECT_TYPES, default=1)
-    status = models.IntegerField(choices=STATUSES, default=1)
+    project_type = models.IntegerField(choices=PROJECT_TYPES, default=2)
+    status = models.IntegerField(choices=STATUSES, default=0)
 
     def __unicode__(self):
         return self.name
+
+    def save(self, *args, **kwargs):
+        if self.logo:
+            width, height = get_image_dimensions(self.logo.file, close=True)
+        else:
+            width, height = None, None
+
+        self.key_image_width = width
+        self.key_image_height = height
+
+        super(Project, self).save(*args, **kwargs)
