9bf39200ef282cbe724e01ff239fa55d6cd92eda
Corey Oordt <coreyoordt@gmail.com>
2012-07-03 20:17:40 -0500

    Updated the projects admin and fixed the urls

--- apps/projects/admin.py
+++ apps/projects/admin.py
@@ -2,11 +2,13 @@ from django.contrib import admin
 
 from projects.models import Project
 
+
 class ProjectAdmin(admin.ModelAdmin):
     list_display = ('name', 'project_type', 'status', 'is_fork')
     list_filter = ('project_type', 'status', )
     search_fields = ('name',)
-
+    actions = ['make_live', 'archive']
+    list_editable = ('status', 'project_type')
     fieldsets = (
         (None, {
             'fields': ('name', 'description', 'logo')
@@ -15,9 +17,14 @@ class ProjectAdmin(admin.ModelAdmin):
             'fields': ('code_url', 'docs_url')
         }),
         ('Info', {
-            'fields': ('is_fork', 'external_id', 'project_type', 'status')
+            'fields': ('is_fork', 'why_forked', 'external_id', 'project_type', 'status')
         })
-)
+    )
 
+    def make_live(self, request, queryset):
+        queryset.update(status=0)
+
+    def archive(self, request, queryset):
+        queryset.update(status=1)
 
 admin.site.register(Project, ProjectAdmin)
